3e1c337d6ebe03cffd2c03c2470538d64dc37e09
Kaji Ryohji <kaji_ryohji@hotmail.com>
2018-08-02 19:05:33 +0200

    proyect2

--- ejercicio.js
+++ ejercicio.js
@@ -1,4 +1,4 @@
-
+/*
 var bands=["green day","u2","lz"];
 var cats = ["Garfield","Silvestre","Tom","Adonis"];
 
@@ -16,3 +16,49 @@ function addBands(array){
 addBands(bands);
 addBands(cats);	
 
+*/
+
+
+		
+/*
+		
+function addMultTable(row,column) {
+	
+			for (var j = 0; j <= row.length; j++){
+				for (var i = 0; i < column.length; i++){
+					
+					console.log(i[j]);
+				
+					
+    	}
+	}
+}
+*/
+
+
+
+
+ function addMultTable(array,array2) {
+        var body = document.getElementsByTagName("body")[0];
+        var table = document.createElement("table");
+	 	var tableBody= document.createElement("tbody");
+        for (var j = 0; j <= array2; j++) {
+            var row = document.createElement("tr");
+			
+            for (var i = 0; i < array; i++) {
+             var col = document.createElement("td");
+				
+                  var colText = document.createTextNode("col is row "+j+", column "+i); 
+
+                col.appendChild(colText);
+                row.appendChild(col);
+            }
+
+            tableBody.appendChild(row);
+			  }
+	 	table.appendChild(tableBody); 
+	 
+        body.appendChild(table);
+
+    }
+	addMultTable(4,8);
---
+++ plusexercises.js
@@ -0,0 +1,9 @@
+
+
+
+function myImage(){
+ var pic =  document.createElement("img");
+	pic.setAttribute("src","C:\Users\kaji_\Desktop\exercises\3.-Random images\images\coss.jpg")
+		imagen.appendChild(pic)
+}
+myImage();
